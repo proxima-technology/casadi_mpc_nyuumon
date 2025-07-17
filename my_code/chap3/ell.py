b3314ac5e263dc1c76e6f71cbc85634ee8e9b674
M-Teranishi-953 <motoya.teranishi@proxima-ai-tech.com>
2025-07-17 20:00:07 +0900

    coef to 3

--- my_code/chap3/ell.py
+++ my_code/chap3/ell.py
@@ -6,7 +6,7 @@ x = casadi.SX.sym('x')
 y = casadi.SX.sym('y')
 
 f = y**2 + x**2  # 最大化したいのでマイナス
-g = y**2 - x**3 + 4*x - 1  # 制約：楕円曲線
+g = y**2 - x**3 + 3*x - 1  # 制約：楕円曲線
 
 nlp = {'x': casadi.vertcat(x, y),
         'f': f,
@@ -22,11 +22,11 @@ print('x_opt:', x_opt)
 X_, Y_ = np.linspace(-4, 4, 400), np.linspace(-4, 4, 400)
 X, Y = np.meshgrid(X_, Y_)
 F = Y**2 + X**2
-C = Y**2 - X**3 + 4*X - 1
+C = Y**2 - X**3 + 3*X - 1
 
 fig, ax = plt.subplots(figsize=(8, 6))
 ax.contour(X, Y, C, levels=[0], colors='black')  # 制約曲線
 ax.contour(X, Y, F, levels=20)  # 等高線
 ax.plot(x_opt[0], x_opt[1], 'ro')
-ax.set_title("Maximize xy on $y^2 = x^3 - 4x + 1$")
+ax.set_title("Maximize xy on $y^2 = x^3 - 3x + 1$")
 plt.show()
